ce58584ff71a76f632f2b85821354918c1a67944
FlorianChazizi <chazizi.florian@gmail.com>
2025-04-13 17:03:03 +0300

    credentials

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "148.251.246.9";
-$username = "exnet8_telesto";
-$password = "Sqa1214WEQ";
-$dbname = "exnet8_telesto";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
